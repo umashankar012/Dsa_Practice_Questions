02a5f21342602c944e6fd9fa8f6f9a2ffa6a72d3
umashankar012 <umashankar8595@gmail.com>
2024-08-04 13:50:03 +0530

    LowerBound

--- Binary_Search/LowerBound.java
+++ Binary_Search/LowerBound.java
@@ -1,49 +1,20 @@
-/*
- Floor in a Sorted Array
+ 
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0;
+        int high = n-1;
+        int ans = n;
 
-Given a sorted array arr[] of size n without duplicates, and given a value x. Floor of x is defined as the largest element k in arr[] such that k is smaller than or equal to x. Find the index of k(0-based indexing).
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid - 1;
 
-Examples
-Input: n = 7, x = 0 arr[] = {1,2,8,10,11,12,19}
-Output: -1
-Explanation: No element less than 0 is found. So output is "-1".
-
-Input: n = 7, x = 5 arr[] = {1,2,8,10,11,12,19}
-Output: 1
-Explanation: Largest Number less than 5 is 2 (i.e k = 2), whose index is 1(0-based indexing).
-Your Task:
-The task is to complete the function findFloor() which returns an integer denoting the index value of K or return -1 if there isn't any such number.
-
-Expected Time Complexity: O(log N).
-Expected Auxiliary Space: O(1).
-
-Constraints:
-1 ≤ n ≤ 107
-1 ≤ arr[i] ≤ 1018
-0 ≤ x ≤ arr[n-1]
- */
-
-
- class Solution {
-
-    // Function to find floor of x
-    // arr: input array
-    // n is the size of array
-    static int findFloor(long arr[], int n, long x) {
-       //edge case
-       if(x < arr[0]){
-        return -1;
-       }
-
-        int indexNumber = -1;
-        //find the just smaller number than x
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] <= x){
-                indexNumber = i;
             }
-        }
-        return indexNumber;
+            else{
+                low = mid + 1;
+            }
+
+        }return ans;
     }
-}
-//Complexity : O(n)  ->> O(1)
 
