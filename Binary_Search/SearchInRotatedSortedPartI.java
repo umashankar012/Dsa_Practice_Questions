e117078864d1d73e00591281c1877bae609537bd
umashankar012 <umashankar8595@gmail.com>
2024-08-05 16:20:16 +0530

    Search In rotated sorted

---
+++ Binary_Search/SearchInRotatedSortedPartI.java
@@ -0,0 +1,88 @@
+/* 
+33. Search in Rotated Sorted Array
+
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+*/
+
+
+//Solution -> 
+
+/*
+ * Apply linear Search to find the element in the array
+ * In worst case, Time Complexity -> O(n)
+ */
+
+ /*
+  *Apply Binary Search
+*/
+  public int search(int[] nums, int target) {
+        
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){
+                return mid; //if equals to the target
+            }
+            // is left sorted
+            if(nums[low] <= nums[mid]){
+                //is element is present in left half
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1; //come in left part
+                }
+                //if not present in left part
+                else{
+                    low = mid + 1;
+                }   
+            }
+
+            //is right sorted
+            else{
+                //check the element is in right part
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = mid + 1; //move to right half
+
+                }
+                //if not in right half
+                else{
+                    high = mid - 1; //move to left half
+                }
+            }
+        }
+        return -1;
+
+    }
+  //T.c -O(logn)
