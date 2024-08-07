bdf3b7381e00ba588ed99001e73bac8c015716f3
umashankar012 <umashankar8595@gmail.com>
2024-08-07 19:36:52 +0530

    peak element

--- Binary_Search/PeakElement.java
+++ Binary_Search/PeakElement.java
@@ -27,14 +27,14 @@ Constraints:
 nums[i] != nums[i + 1] for all valid i.
  */
 
- //Solution 1:: 
- /*
-  Traverse using a for loop variable (say i)
-    and checks If ((i == 0 or arr[i-1] < arr[i]) and (i == n-1 or arr[i] > arr[i+1]))
-    return i
+//Solution 1:: 
+/*
+ Traverse using a for loop variable (say i)
+   and checks If ((i == 0 or arr[i-1] < arr[i]) and (i == n-1 or arr[i] > arr[i+1]))
+   return i
 
 T.C :: O(n)
-  */
+ */
 
 //Solution 2 ::
 /*
@@ -42,33 +42,32 @@ T.C :: O(n)
  * 
  */
 
- public int findPeakElement(int[] nums) {
+public int findPeakElement(int[] nums) {
     int n = nums.length;
-    if(n == 1){
+    if (n == 1) {
         return 0;
     }
-    if(nums[0] > nums[1]){
+    if (nums[0] > nums[1]) {
         return 0;
     }
 
-    if(nums[n-1] > nums[n-2]){
-        return n-1;
+    if (nums[n - 1] > nums[n - 2]) {
+        return n - 1;
     }
 
     int low = 1;
-    int high = n-2;
+    int high = n - 2;
 
-    while(low <= high){
-        int mid = low + (high - low)/2;
-        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
             return mid;
         }
 
-        else if(nums[mid] > nums[mid-1]){
+        else if (nums[mid] > nums[mid - 1]) {
             low = mid + 1;
-        }
-        else{
-            high = mid-1;
+        } else {
+            high = mid - 1;
         }
     }
     return -1;
